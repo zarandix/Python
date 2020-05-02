968a99edc319ef336c15efa49fbe4bdf1631a3e5
zarandix <64537224+zarandix@users.noreply.github.com>
2020-05-02 19:03:03 +0430

    Update README.py

--- README.py
+++ README.py
@@ -1,3 +1,3 @@
-print "hi my friends"
-print "this is a personal account for share,debugging and commenting by my friends."
-print "thanks for attention"
+print ("hi my friends")
+print ("this is a personal account for share,debugging and commenting by my friends.")
+print ("thanks for attention")
